2f74ca7c451962c8f8f4f4d5b97eaabf8ce64279
Kayla Cross <kac288@georgetown.edu>
2018-09-07 19:24:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,7 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do
     @recipe = Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     if @recipe
+      @recipe.save
       redirect to '/recipes'
     else
       redirect to '/recipes/new'
